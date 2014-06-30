0040fe00eae3f2ccd5c3f962ebcab7da3e38e060
buboiasinine <buboiasinine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-30 05:27:49 +0000

    commit 1.0.11
    git-svn-id: https://plugins.svn.wordpress.org/page-speed/trunk@940657 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,77 +1,79 @@
-	=== Plugin Name ===
-Contributors: buboiasinine
-Donate link: 
-Tags: Page Speed, Goole Page Speed Insights, PageSpeed Insights, Css, Js, Javascript, Optimize Css, Remove render-blocking JavaScript, Move Script In Footer, Eliminate render-blocking JavaScript and CSS in above-the-fold content  
-Requires at least: 3.0
-Tested up to: 3.9
-Stable tag: 1.0.10
-License: GPLv2 or later
-License URI: GPLv2 or later
+	=== Plugin Name ===
+Contributors: buboiasinine
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NAX45XQV5P7ZW
+Tags: Page Speed, Goole Page Speed Insights, PageSpeed Insights, Css, Js, Javascript, Optimize Css, Remove render-blocking JavaScript, Move Script In Footer, Eliminate render-blocking JavaScript and CSS in above-the-fold content  
+Requires at least: 3.0
+Tested up to: 3.9
+Stable tag: 1.0.11
+License: GPLv2 or later
+License URI: GPLv2 or later
+
+Page Speed Plugin Allows You To Eliminate render-blocking JavaScript and CSS in above-the-fold content
+
+== Description ==
+
+Blocking Script and Css Resources Can Cause a Delay in Your Page.
+
+For More Info Visit <a href="https://developers.google.com/speed/docs/insights/about" target="_blank"> Google PageSpeed Insights </a>
+
+
+Features include: 
+
+Eliminate render-blocking JavaScript and CSS in above-the-fold content
+
+
+
+*   Remove render-blocking JavaScript
+*  Optimize CSS Delivery 
+
+Future Development: 
+
+*   Leverage browser caching
+*  Minify JavaScript
+* Minify CSS
+* Optimize images
+
+
+I Will Update This Plugin for more simplier option and more functionality as soon as possible..
+cheers...
+
+
+== Installation ==
+
+
+
+1. Upload `Page Speed` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Configure the plugin 'Admin' 'Page Speed' 
+
+== Frequently Asked Questions ==
+
+= Where can i find the Script Name  i want to remove =
+
+This Will Help <a target="_blank" href="http://codex.wordpress.org/Function_Reference/wp_enqueue_script#Notes"> Click Me! </a>
+
+Moving the js to footer is not easy.. 
+
+you need to dig deep to the plugin folder to find what is the js name of the script you want to move in the footer.
+
+once you find the js name you want to move copy paste first all the script to the page speed plugin then put te js name to the "js name field".
+
+dont worry i will update this plugin for more easier option as soon as posible..
+
+= Where can i find the Css Name of the plugin i want to remove =
+
+Visit your site, right click and view page source
+Find the Id of the Css you want to Optimize, remove the "-css" ex. flexslider-css to flexslider
+and put it in the Page Speed Plugin, Css Name
+
+
+Dont Forget to Copy and Paste The Css that you've been remove in the Css Field inside the plugin
+
+== Screenshots ==
 
-Page Speed Plugin Allows You To Eliminate render-blocking JavaScript and CSS in above-the-fold content
-
-== Description ==
-
-Blocking Script and Css Resources Can Cause a Delay in Your Page.
-
-For More Info Visit <a href="https://developers.google.com/speed/docs/insights/about" target="_blank"> Google PageSpeed Insights </a>
-
-
-Features include: 
-
-Eliminate render-blocking JavaScript and CSS in above-the-fold content
-
-
-
-*   Remove render-blocking JavaScript
-*  Optimize CSS Delivery 
-
-Future Development: 
-
-*   Leverage browser caching
-*  Minify JavaScript
-* Minify CSS
-* Optimize images
-
-
-I Will Update This Plugin for more simplier option and more functionality as soon as possible..
-cheers...
-
-
-== Installation ==
-
-
-
-1. Upload `Page Speed` to the `/wp-content/plugins/` directory
-2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Configure the plugin 'Admin' 'Page Speed' 
-
-== Frequently Asked Questions ==
-
-= Where can i find the Script Name  i want to remove =
-
-This Will Help <a target="_blank" href="http://codex.wordpress.org/Function_Reference/wp_enqueue_script#Notes"> Click Me! </a>
-
-Moving the js to footer is not easy.. 
-
-you need to dig deep to the plugin folder to find what is the js name of the script you want to move in the footer.
-
-once you find the js name you want to move copy paste first all the script to the page speed plugin then put te js name to the "js name field".
-
-dont worry i will update this plugin for more easier option as soon as posible..
-
-= Where can i find the Css Name of the plugin i want to remove =
-
-Visit your site, right click and view page source
-Find the Id of the Css you want to Optimize, remove the "-css" ex. flexslider-css to flexslider
-and put it in the Page Speed Plugin, Css Name
-
-
-Dont Forget to Copy and Paste The Css that you've been remove in the Css Field inside the plugin
-
-== Screenshots ==
-
-
-Settings page
-
+<img src="/tags/1.0.11/ps-screenshot-1.png">
+
+
+Settings page
+
 == Changelog ==
--- settings_page.php
+++ settings_page.php
@@ -1,226 +1,491 @@
 <?php
+
 // create custom plugin settings menu
+
 add_action('admin_menu', 'pg_pagespeed_menu');
+
 function pg_pagespeed_menu() {
+
 //create new top-level menu
+
 add_menu_page('Page Speed Plugin Settings', 'Page Speed', 'administrator', __FILE__, 'ps_pagespeed_settings_page',plugins_url('/images/page_speed_icon.png', __FILE__));
+
 }
+
 //call register settings function
+
 add_action( 'admin_init', 'pg_pagespeed_register_mysettings' );
+
+
 
 add_action( 'wp_print_styles', 'pg_pagespeed_dequeue_my_css', 100 );
 
+
+
 add_action( 'wp_print_scripts', 'pg_pagespeed_dequeue_my_js', 100 );
 
+
+
 // inline css 
+
 add_action('wp_head','pg_pagespeed_insert_inline_css');
 
+
+
 // inline js 
+
 add_action('wp_footer','pg_pagespeed_inline_js');
 
+
+
 function pg_pagespeed_register_mysettings() {
+
 	//register our settings
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name1' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name2' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name3' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name4' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name5' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name6' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name7' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name8' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name9' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name10' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name11' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name12' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name13' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name14' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_css_name15' );
+
 	// register js
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name1' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name2' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name3' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name4' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name5' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name6' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name7' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name8' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name9' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name10' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name11' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name12' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name13' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name14' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_js_name15' );
+
 	// register in option
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_inline_css' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'insert_inline_js' );
+
 	
+
 	register_setting( 'ps_pagespeed-settings-group', 'move_all_script_infooter' );
+
 	register_setting( 'ps_pagespeed-settings-group', 'inline_the_css' );	
+
 	register_setting( 'ps_pagespeed-settings-group', 'activate_gzip' );		
 
+
+
 	
+
 	}
+
 function ps_pagespeed_settings_page() {
+
 ?>
+
 <style> 
 
+
+
 .inline-wrap {
+
     display: inline-block;
+
     margin-right: 10px;
+
     vertical-align: top;
+
 }
+
+
 
 .gzip-inactive {
+
     font-weight: bold;
+
 }
+
+
 
 .gzip-active {
+
     color: #008000;
+
     font-weight: bold;
+
 }
+
 .col2 {
+
 display: inline-block;
+
 vertical-align: top;
+
 }
+
 .ps-textarea {
+
 min-height: 450px;
+
 }
+
 .js-css-name-wrap {
+
 color:green;
+
 }
+
+.ps-content-wrapper {
+width: 88%;
+float:left;
+}
+
+
+.donation-wrap {
+float: right;
+width: 10%;
+}
+
+
+input[type="image"] {
+width: 100%;
+width: 1inherit;
+}
+
 </style>
+
 <div class="wrap">
+
+<div class="ps-content-wrapper">
+
 <h1> Page Speed Settings</h1>
+
 <h2> Eliminate render-blocking JavaScript and CSS in above-the-fold content </h2>
+
 <form method="post" action="options.php">
+
     <?php settings_fields( 'ps_pagespeed-settings-group' ); ?>
+
     <?php do_settings_sections( 'ps_pagespeed-settings-group' ); ?>
+
   <div>
 
+
+
 		<h3> Remove render-blocking JavaScript:  </h3> 
+
 		
+
 		
+
 	<div class="inline-wrap">
+
 <p> Insert The Js Name Here </p>
+
 <div class="col2">
+
 	<p><input type="text" name="insert_js_name1" value="<?php echo get_option('insert_js_name1'); ?>" /></p>
+
 	<p><input type="text" name="insert_js_name2" value="<?php echo get_option('insert_js_name2'); ?>" /></p>
+
 	<p><input type="text" name="insert_js_name3" value="<?php echo get_option('insert_js_name3'); ?>" /></p>
+
 	<p><input type="text" name="insert_js_name4" value="<?php echo get_option('insert_js_name4'); ?>" /></p>
+
 	<p><input type="text" name="insert_js_name5" value="<?php echo get_option('insert_js_name5'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name6" value="<?php echo get_option('insert_js_name6'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name7" value="<?php echo get_option('insert_js_name7'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name8" value="<?php echo get_option('insert_js_name8'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name9" value="<?php echo get_option('insert_js_name9'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name10" value="<?php echo get_option('insert_js_name10'); ?>" /></p>
+
     </div>
+
     <div class="col2">
+
     <p><input type="text" name="insert_js_name11" value="<?php echo get_option('insert_js_name11'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name12" value="<?php echo get_option('insert_js_name12'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name13" value="<?php echo get_option('insert_js_name13'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name14" value="<?php echo get_option('insert_js_name14'); ?>" /></p>
+
     <p><input type="text" name="insert_js_name15" value="<?php echo get_option('insert_js_name15'); ?>" /></p>
+
     </div>
+
 	</div>
+
 	<div class="inline-wrap">
+
 	<p> Copy and Paste The Js File Here:  </p>
+
 	<p><textarea class="ps-textarea" rows="4" cols="50" name="insert_inline_js">  <?php  echo get_option('insert_inline_js'); ?> </textarea> </p>
+
 	</div>
+
 	<hr>
+
 	<h3> Optimize CSS Delivery  </h3> 
+
 	<div class="inline-wrap">
+
 	<p> Insert The Css File Name Here: </p>
+
     <div class="col2">
+
 	<p><input type="text" name="insert_css_name1" value="<?php echo get_option('insert_css_name1');  ?>" /></p>
+
 	<p><input type="text" name="insert_css_name2" value="<?php echo get_option('insert_css_name2');  ?>" /></p>
+
 	<p><input type="text" name="insert_css_name3" value="<?php echo get_option('insert_css_name3');  ?>" /></p>
+
 	<p><input type="text" name="insert_css_name4" value="<?php echo get_option('insert_css_name4');  ?>" /></p>
+
 	<p><input type="text" name="insert_css_name5" value="<?php echo get_option('insert_css_name5');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name6" value="<?php echo get_option('insert_css_name6');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name7" value="<?php echo get_option('insert_css_name7');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name8" value="<?php echo get_option('insert_css_name8');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name9" value="<?php echo get_option('insert_css_name9');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name10" value="<?php echo get_option('insert_css_name10');  ?>" /></p>
+
     </div>
+
     <div class="col2">
+
     <p><input type="text" name="insert_css_name11" value="<?php echo get_option('insert_css_name11');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name12" value="<?php echo get_option('insert_css_name12');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name13" value="<?php echo get_option('insert_css_name13');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name14" value="<?php echo get_option('insert_css_name14');  ?>" /></p>
+
     <p><input type="text" name="insert_css_name15" value="<?php echo get_option('insert_css_name15');  ?>" /></p>
+
     </div>
+
 </div>
+
 <div class="inline-wrap">
+
 <p> Copy and Paste the Css Here: </p>
+
 	<p><textarea class="ps-textarea" rows="4" cols="50" name="insert_inline_css">  <?php  echo get_option('insert_inline_css'); ?> </textarea> </p>
+
 </div>
+
+
 
 <hr>
 
 
+
+
+
 	
 
+
+
 	</div>
+
     <?php submit_button(); ?>
+
 </form>
+
+
+
+
 
 
 
 <p> Below is the name of the js and css used in this site, use the path/url to get the script/stylesheet and copy-paste it in the field above. </p>
 
 
+
+
+
   <h3>JS </h3>
 
+
+
 <?php
+
   global $wp_scripts, $wp_styles;
+
    
+
 	 foreach( $wp_scripts->done as $ps_scripts ) :  
+
 	
+
 ?>
+
+
 
 <div class="hint-wrapper">
 
+
+
 <div class="js-css-name-wrap"> <?php echo $ps_scripts; ?> </div>
+
 	
+
 <div class="js-css-path-wrap"> <?php echo $wp_scripts->registered[$ps_scripts ]->src; ?> </div>
 
+
+
 </div>
+
 <?php
+
 	
 
+
+
 		endforeach;
+
 		
+
 ?> 
+
+
 
 <h3> CSS  </h3>
 
+
+
 <?php
+
 	     foreach( $wp_styles->done as $handle ) :
+
 		 
+
 		 if ($wp_styles->registered[$handle]->args == 'all') {
+
 		 
+
 		 ?>
+
          
+
          <div class="hint-wrapper">
 
+
+
 <div class="js-css-name-wrap"> <?php echo $handle; ?> </div>
+
 	
+
 <div class="js-css-path-wrap"> <?php echo $wp_styles->registered[$handle]->src; ?> </div>
 
+
+
 </div>
+
          <?php
+
 	}
+
 	
+
 	
+
     endforeach;
+
 		
+
 ?>
 
+</div>
+
+
+
+<div class="donation-wrap">
+
+<h2 style="color:red; text-align:center;"> 
+<a target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NAX45XQV5P7ZW">
+DONATION 
+</a>
+</h2>
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="NAX45XQV5P7ZW">
+<input type="image" src="https://lh3.googleusercontent.com/-Y9jfoDnO0fw/U7CvsTaPEWI/AAAAAAAAAKs/aIukBvl7TLQ/w426-h554/donation.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+
+<a style="text-align:center;" target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NAX45XQV5P7ZW">
+ Support Wordpress Developers 
+</a>
+
+</div>
+
 
 
 
 </div>
+
 <?php 
+
 } 
+
 ?>
